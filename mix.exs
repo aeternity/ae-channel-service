df37e48ab1c91f6bb26680a4a39251ec26ac2359
Aleksandar Filipov <aleksandar.filipov@jayway.com>
2019-05-23 11:36:58 +0200

    clean out deps

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AeChannelService.MixProject do
   def project do
     [
       apps_path: "apps",
-      # apps: [:aetx, :aesocketconnector],
+      # apps: [:aesocketconnector],
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -17,28 +17,6 @@ defmodule AeChannelService.MixProject do
   # Run "mix help deps" for examples and options.
   defp deps do
     [
-      {:poison, "~> 3.1"},
-      {:websockex, "~> 0.4.0"},
-      {:enacl, git: "https://github.com/aeternity/enacl.git", ref: "26180f4"},
-      {:aeserialization, git: "https://github.com/aeternity/aeserialization.git"},
-      # # {:aeminer, git: "https://github.com/aeternity/aeminer.git", manager: :drebar3},
-      # # {:aeminer, git: "https://github.com/aeternity/aeminer.git", manager: :none, app: false, override: true, compile: false},
-      # {:aeminer, path: "../aeminer", manager: :rebar3, override: true},
-      # {:aeternity, git: "https://github.com/aeternity/aeternity"},
-      # # {:aebytecode, ">= 0.0.0", [env: :prod, override: true, git: "https://github.com/aeternity/aebytecode.git", ref: "2f4e188", manager: :rebar3, compile: false]},
-      # {:aebytecode, path: "../aebytecode", manager: :rebar3, compile: false, override: true},
-      # # {:aebytecode, ">= 0.0.0", [env: :prod, override: true, git: "https://github.com/aeternity/aebytecode.git", ref: "2f4e188", manager: :rebar3]},
-      # {:aeserialization, ">= 0.0.0", [env: :prod, override: true, git: "https://github.com/aeternity/aeserialization.git", ref: "3416ff5", manager: :rebar3]},
-      # {:trace_runner, [env: :prod, override: true, git: "git://github.com/uwiger/trace_runner.git", ref: "303ef2f", manager: :rebar3]},
-      # # {:trace_runner, ">= 0.0.0", [env: :prod, override: true, git: "git://github.com/uwiger/trace_runner.git", ref: "303ef2f", manager: :rebar3]},
-      # {:sext, git: "https://github.com/uwiger/sext.git", ref: "07a4c2d66", override: true},
-      # # {:sext, git: tag: "07d5d516", manager: :rebar, override: true},
-      # # {:sext, "1.5.0", tag: "07d5d516", override: true, compile: false},
-      # # # {:mnesia_leveled, ">= 0.0.0", [env: :prod, override: true, git: "https://github.com/aeternity/mnesia_leveled.git", ref: "86e78b7", manager: :rebar3, compile: false]},
-      # # # {:mnesia_rocksdb, ">= 0.0.0", [env: :prod, override: true, git: "https://github.com/aeternity/mnesia_rocksdb.git", ref: "ad8e7b6", manager: :rebar3, compile: false]},
-      # {:mnesia_leveled, [env: :prod, override: true, git: "https://github.com/aeternity/mnesia_leveled.git", ref: "86e78b7", manager: :rebar3]},
-      # {:mnesia_rocksdb, [env: :prod, override: true, git: "https://github.com/aeternity/mnesia_rocksdb.git", ref: "ad8e7b6", manager: :rebar3]},
-      # {:lz4, ">= 0.0.0", [env: :prod, override: true, git: "https://github.com/aeternity/erlang-lz4.git", ref: "1ff9f36", manager: :rebar3]}
     ]
   end
 end
