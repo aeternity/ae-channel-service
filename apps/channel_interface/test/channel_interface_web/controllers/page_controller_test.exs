cb5a7668820cf749cd12fc778300b9de36527601
Aleksandar Filipov <aleksandar.filipov@jayway.com>
2019-12-17 13:27:39 +0100

    phoenix added, start with: iex -S mix phx.server

---
+++ apps/channel_interface/test/channel_interface_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule ChannelInterfaceWeb.PageControllerTest do
+  use ChannelInterfaceWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, "/")
+    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  end
+end
