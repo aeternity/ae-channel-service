bfc3e8fb0cd6e21ed7dcee4ce791e16a691fb1b7
Aleksandar Filipov <aleksandar.filipov@jayway.com>
2020-03-05 10:18:20 +0100

    verbose logging during test, changed some log levels

--- apps/channel_interface/config/test.exs
+++ apps/channel_interface/config/test.exs
@@ -7,4 +7,4 @@ config :channel_interface, ChannelInterfaceWeb.Endpoint,
   server: false
 
 # Print only warnings and errors during test
-config :logger, level: :warn
+# config :logger, level: :warn
