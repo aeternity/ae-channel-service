4db05db5eaacc0d8f7e8b6060c85925fe9f99a0d
Aleksandar Filipov <aleksandar.filipov@jayway.com>
2020-03-23 15:18:09 +0100

    supervise all backend channels

--- apps/ae_backend_service/lib/backend_service_manager.ex
+++ apps/ae_backend_service/lib/backend_service_manager.ex
@@ -9,7 +9,8 @@ defmodule BackendServiceManager do
   use GenServer
   require Logger
 
-  defstruct pid_session_holder: nil
+  defstruct pid_session_holder: nil,
+            channel_id_table: %{}
 
   # Client
 
@@ -17,15 +18,23 @@ defmodule BackendServiceManager do
     GenServer.start_link(__MODULE__, {}, name: __MODULE__)
   end
 
+  # only one instance of the manager.
   def start_channel(params) do
     GenServer.call(__MODULE__, {:start_channel, params})
   end
 
+  def get_channel_id(pid, identifier) do
+    GenServer.call(pid, {:get_channel_id, identifier})
+  end
+
+  def set_channel_id(pid, identifier, channel_id) do
+    GenServer.call(pid, {:set_channel_id, identifier, channel_id})
+  end
 
 
   # Server
   def init({}) do
-    {:ok, %__MODULE__{}}
+    {:ok, %__MODULE__{channel_id_table: %{}}}
   end
 
   defp is_reestablish(reestablish) do
@@ -36,25 +45,44 @@ defmodule BackendServiceManager do
     end
   end
 
-  defp start_channel_local(
-        {
-          role,
-          _config,
-          {channel_id, reestablish_port} = reestablish,
-          _keypair_initiator
-        } = params
-      )
-      when role in [:initiator, :responder] do
-    if is_reestablish(reestablish) do
-      # only superview reestablished sessions, no way to relocate a fsm without channel_id and fsm_id
-      Supervisor.start_child(ChannelSupervisor.Supervisor, [{params, self()}])
-    else
-      BackendSession.start_link({params, self()})
-    end
+  # defp start_channel_local(
+  #       {
+  #         role,
+  #         _config,
+  #         {channel_id, reestablish_port} = reestablish,
+  #         _keypair_initiator
+  #       } = params
+  #     )
+  #     when role in [:initiator, :responder] do
+  #   identifier = :erlang.unique_integer([:monotonic])
+  #   if is_reestablish(reestablish) do
+  #     # only superview reestablished sessions, no way to relocate a fsm without channel_id and fsm_id
+  #     Supervisor.start_child(ChannelSupervisor.Supervisor, [{params, {self()}, identifier}])
+
+  #   # else
+  #   #   BackendSession.start_link({params, self()})
+  #   # end
+  # end
+
+  def handle_call({:get_channel_id, identifier}, _from, state) do
+    Logger.info("got channel id: #{inspect Map.get(state.channel_id_table, identifier, {"", 0})}")
+    {:reply, Map.get(state.channel_id_table, identifier, {"", 0}), state}
   end
 
-  def handle_call({:start_channel, params}, _from, state) do
-    {:ok, pid} = start_channel_local(params)
-    {:reply, pid, state}
+  def handle_call({:set_channel_id, identifier, reestablish}, _from, state) do
+    Logger.info("Channel poulated with channel_id #{inspect {reestablish, identifier}}")
+    {:reply, :ok, %__MODULE__{state | channel_id_table: Map.put(state.channel_id_table, identifier, reestablish)}}
+  end
+
+  def handle_call({:start_channel, {_role, _config, reestablish, _keypair_initiator} = params}, _from, state) do
+    # {:ok, pid} = start_channel_local(params)
+    identifier = :erlang.unique_integer([:monotonic])
+    {:ok, pid} = Supervisor.start_child(ChannelSupervisor.Supervisor, [{params, {self(), identifier}}])
+    case is_reestablish(reestablish) do
+      true ->
+        {:reply, pid, %__MODULE__{state | channel_id_table: Map.put(state.channel_id_table, identifier, reestablish)}}
+      false ->
+        {:reply, pid, state}
+    end
   end
 end
--- apps/ae_backend_service/lib/backend_session.ex
+++ apps/ae_backend_service/lib/backend_session.ex
@@ -19,12 +19,14 @@ defmodule BackendSession do
 
 
   defstruct pid_session_holder: nil,
-            pid_backend_manager: nil
+            pid_backend_manager: nil,
+            identifier: nil,
+            port: nil
 
   #Client
 
-  def start_link({params, pid_manager}) do
-    GenServer.start_link(__MODULE__, {params, pid_manager})
+  def start_link({params, {_pid_manager, _identifier} = manager_data}) do
+    GenServer.start_link(__MODULE__, {params, manager_data})
   end
 
   defp log_callback({type, message}) do
@@ -35,10 +37,16 @@ defmodule BackendSession do
   end
 
   #Server
-  def init({{role, channel_config, {_channel_id, _reestablish_port} = reestablish, initiator_keypair} = params, pid_manager}) do
-    Logger.info("Starting backend session #{inspect params} pid is #{inspect self()}")
+  def init({params, {pid_manager, identifier}}) do
+    Logger.info("Starting backend session #{inspect {params, identifier}} pid is #{inspect self()}")
+    GenServer.cast(self(), {:resume_init, params})
+    {:ok, %__MODULE__{pid_backend_manager: pid_manager, identifier: identifier}}
+  end
+
+  def handle_cast({:resume_init, {role, channel_config, _reestablish, initiator_keypair}}, state) do
+    {_channel_id, port} = reestablish = BackendServiceManager.get_channel_id(state.pid_backend_manager, state.identifier)
     {:ok, pid} = SessionHolderHelper.start_session_holder(role, channel_config, reestablish, initiator_keypair, fn -> keypair_responder() end, SessionHolderHelper.connection_callback(self(), :blue, &log_callback/1))
-    {:ok, %__MODULE__{pid_session_holder: pid, pid_backend_manager: pid_manager}}
+    {:noreply, %__MODULE__{state | pid_session_holder: pid, port: port}}
   end
 
   #TODO once we know the channel_id this process should register itself somewhere.
@@ -47,7 +55,11 @@ defmodule BackendSession do
   end
 
   # TODO backend just happily signs
-  def handle_cast({:match_jobs, {:sign_approve, _round, _round_initiator, method, _channel_id}, to_sign} = _message, state) do
+  def handle_cast({:match_jobs, {:sign_approve, _round, _round_initiator, method, channel_id}, to_sign} = _message, state) do
+    if (method == "channels.sign.responder_sign") do
+      BackendServiceManager.set_channel_id(state.pid_backend_manager, state.identifier, {channel_id, state.port})
+    end
+
     signed = SessionHolder.sign_message(state.pid_session_holder, to_sign)
     fun = &SocketConnector.send_signed_message(&1, method, signed)
     SessionHolder.run_action(state.pid_session_holder, fun)
@@ -59,8 +71,8 @@ defmodule BackendSession do
     {:noreply, state}
   end
 
-  def handle_cast(message, socket) do
+  def handle_cast(message, state) do
     Logger.warn("unprocessed message received in backend #{inspect(message)}")
-    {:noreply, socket}
+    {:noreply, state}
   end
 end
