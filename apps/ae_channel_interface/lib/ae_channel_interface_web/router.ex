6a65ca737d0c45fb49d9f6d0d99ac9e3c07da8ab
Artur64 <artois641@gmail.com>
2020-04-27 19:25:14 +0300

    Introducing backend active channels id page

--- apps/ae_channel_interface/lib/ae_channel_interface_web/router.ex
+++ apps/ae_channel_interface/lib/ae_channel_interface_web/router.ex
@@ -17,6 +17,7 @@ defmodule AeChannelInterfaceWeb.Router do
     pipe_through(:browser)
 
     get("/", PageController, :index)
+    get("/channels", PageController, :show)
     resources("/connect", ConnectController, only: [:new])
   end
 
